cb3f25c3e4ed7416252e6099aca724a5a42ec6ac
Dalia <dalia.bachmatiuk@codewave.pl>
2025-06-06 16:16:32 +0200

    add field listeners only to multilingual schema

--- plugins/field-listeners/index.js
+++ plugins/field-listeners/index.js
@@ -1,34 +1,36 @@
-export const handleFormFieldListenrsAdd = ({ formik, name }) => {
-  if (!name) return;
+import pluginInfo from "../../plugin-manifest.json";
 
-  if (name === "languages") {
-    return {
-      onChange: ({ value }) => {
-        if (value.length === 0) {
-          formik.setFieldValue("default_language", "");
-        }
-        formik.setFieldValue(name, value);
-      },
-    };
-  } else if (name === "content_types") {
-    return {
-      onChange: ({ value }) => {
-        let sliceIndex = null;
-        const lastIndex = value.length - 1;
-
-        if (lastIndex > 0) {
-          if (value[0] === "--all--") {
-            sliceIndex = 1;
-          } else if (value[lastIndex] === "--all--") {
-            sliceIndex = lastIndex;
+export const handleFormFieldListenrsAdd = ({ contentType, formik, name }) => {
+  if (name && contentType?.id === pluginInfo.id && contentType?.nonCtdSchema) {
+    if (name === "languages") {
+      return {
+        onChange: ({ value }) => {
+          if (value.length === 0) {
+            formik.setFieldValue("default_language", "");
           }
-        }
+          formik.setFieldValue(name, value);
+        },
+      };
+    } else if (name === "content_types") {
+      return {
+        onChange: ({ value }) => {
+          let sliceIndex = null;
+          const lastIndex = value.length - 1;
 
-        formik.setFieldValue(
-          name,
-          sliceIndex ? value.slice(sliceIndex) : value,
-        );
-      },
-    };
+          if (lastIndex > 0) {
+            if (value[0] === "--all--") {
+              sliceIndex = 1;
+            } else if (value[lastIndex] === "--all--") {
+              sliceIndex = lastIndex;
+            }
+          }
+
+          formik.setFieldValue(
+            name,
+            sliceIndex ? value.slice(sliceIndex) : value,
+          );
+        },
+      };
+    }
   }
 };
